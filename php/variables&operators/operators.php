1bf44cba54f1690a06717e37e4d1529f9f585a03
Aryan <aryangupta8092@gmail.com>
2024-06-23 22:54:46 +0530

    operators

---
+++ php/variables&operators/operators.php
@@ -0,0 +1,21 @@
+<?php
+    echo 10+5 . "\n"; // space
+    echo 10-5 . "<br>"; // next line
+    echo 10*5 . "<br>";
+    echo 10/5 . "<br>";
+    echo 10%3 . "<br>";
+    $a =15;
+    echo$a . "<br>";
+    $a +=15;
+    echo $a . "<br>";
+    $a=15;
+    $a-=15;
+    echo $a . "<br>";
+
+    $a=5;
+    $a++;
+    echo $a . "<br>";
+    $a=5;
+    $a--;
+    echo $a . "<br>";
+?>
