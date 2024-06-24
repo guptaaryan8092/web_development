90902d1d5419c00d16c87732712a538ce372645e
Aryan <aryangupta8092@gmail.com>
2024-06-24 18:17:47 +0530

    array

--- php/array/array_foreachloop.php
+++ php/array/array_foreachloop.php
@@ -1,5 +1,5 @@
 <?php
     $presidents = array("George","Barack","Donald");
-    foreach( $presidents as $string)
-    echo $string." ";
+    foreach( $presidents as $name)
+    echo $name." ";
 ?>
---
+++ php/array/associative_array.php
@@ -0,0 +1,6 @@
+<?php
+$years = array("George"=> 2001,"Barack" =>2009,"Donald" =>2017);
+foreach ($years as $name => $year) {
+    echo $name . " was first elected in " . $year . "\n" ."<br>";
+}
+?>
