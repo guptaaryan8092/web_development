9a2b7f761822c85c50e5fa57203f67ef75c196a7
Aryan <aryangupta8092@gmail.com>
2024-06-25 16:01:56 +0530

    added_comment

--- php/string/escape_character.php
+++ php/string/escape_character.php
@@ -4,5 +4,6 @@
     echo "Hope you are having a great time.";
     echo "Hello \$name!.\n";
     echo "Hello \"$name!.\"\n";
+    echo 'Hello $name!.\n'; // use of single quotes. It will print as it is.
 
 ?>
