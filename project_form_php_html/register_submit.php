3ee280175d153112524758e52c10aa859e9b8576
Aryan <aryangupta8092@gmail.com>
2024-06-26 23:09:41 +0530

    form

---
+++ project_form_php_html/register_submit.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    echo $name . "<br/>";
+    echo $email . "<br/>";
+    echo $password . "<br/>";
+?>
