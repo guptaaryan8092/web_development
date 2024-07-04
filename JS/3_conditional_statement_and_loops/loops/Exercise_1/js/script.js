db87cd32367244badab2d570fa20012851ba89e3
Aryan <aryangupta8092@gmail.com>
2024-07-04 23:52:50 +0530

    loop

---
+++ JS/3_conditional_statement_and_loops/loops/Exercise_1/js/script.js
@@ -0,0 +1,11 @@
+//This code will correctly print even numbers from 1 to 10:
+let n;
+    for(n=1;n<=10;n++)
+    {
+        if(n%2==0);
+        {
+            document.write(n + '<br>');
+        }
+
+    }
+        
