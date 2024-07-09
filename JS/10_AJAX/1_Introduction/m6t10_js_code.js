e9a7ff0a4a13979a4b31afdee4dece88078ab79f
Aryan <aryangupta8092@gmail.com>
2024-07-10 01:29:51 +0530

    ajax

---
+++ JS/10_AJAX/1_Introduction/m6t10_js_code.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let httpRequest = new XMLHttpRequest();
+
+let el = document.getElementById("abc");
+el.onclick = function() {
+    el.innerHTML = "This is some new content.";
+    el.style.color = "red";
+    
+    httpRequest.onreadystatechange = postAjaxFunction;
+    httpRequest.open('POST', 'update_database.php');
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    httpRequest.send('email=aditya@internshala.com&id=4');
+}
+
+
+function postAjaxFunction() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            var response = JSON.parse(httpRequest.responseText);
+            alert(response.name + " - " + response.message);
+        } else {
+            alert('Something went wrong.');
+        }
+    }
