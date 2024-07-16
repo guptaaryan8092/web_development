8382d8d36a47e8b47ee4c34431bfcac738214496
Aryan <aryangupta8092@gmail.com>
2024-07-16 18:04:09 +0530

    path change

--- project_form_php_html/register_submit.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    echo $name . "<br/>";
-    echo $email . "<br/>";
-    echo $password . "<br/>";
-?>
