dffda28d0b01604da05ff871e62b718eb047249b
Aryan <aryangupta8092@gmail.com>
2024-07-17 14:21:37 +0530

    php name change

---
+++ php/project_form_php_html/login_submit.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+    $db_hostname = "127.0.0.1";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "test";
+
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+    if (!$conn) {
+        echo "Connection failed: " . mysqli_connect_error();
+        exit;
+    }
+
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        echo "Error: " . mysqli_error($conn);
+        exit;
+    }
+
+    $row = mysqli_fetch_assoc($result);
+    if ($row) {
+        echo "Hello " . $row['name'] . "<br/>";
+    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['name'] = $row['name'];
+
+    //setcookie("user_id", $row['id'],time()+3600); // id use because it is unique
+    //setcookie('name', $row['name'],time()+ 3600);
+
+        ?>
+        <a href="dashboard.php">Click Here</a>;
+        <?php
+    } else {
+        echo "Login Failed<br/>";
+    }
+
+    mysqli_close($conn);
+?>
