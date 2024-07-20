db091af50f3b3d880802c65e5c55f05097926516
Aryan <aryangupta8092@gmail.com>
2024-07-21 02:43:06 +0530

    expression in jsx

---
+++ react/3_JSX/2_Expression_in_jsx/1_/js/app.js
@@ -0,0 +1,15 @@
+const name ="Aryan"
+const box = (
+    <div className='box'>
+      <h1 id='abc'>This is {name}'s heading</h1>
+      <p>This is a test paragraph which is created by React. 
+         This paragraph has no attribute and is contained inside a div which is also created by React.</p>
+    </div>
+  );
+  
+  ReactDOM.render(
+    box,
+  
+    document.getElementById('react-container')
+  );
+  
